1e2a646bba15e2bbbe53efcd2b4c731b50c9ce42
Robert Medina <robertsmedina11@gmail.com>
2024-05-05 11:14:24 -0400

    module 1 project complete

--- frontend/components/tests/App.test.js
+++ frontend/components/tests/App.test.js
@@ -17,9 +17,11 @@ describe('Stranger Things App', () => {
   })
   test('App mounts without crashing', () => {
     // 👉 TASK: print the simulated DOM using screen.debug
+    screen.debug()
   })
   test('App renders the correct texts', async () => {
     // 👉 TASK: click on the button that displays "Press to Get Show Data"
+    user.click(screen.getByText('Press to Get Show Data'))
 
     // 👉 TASK: create a waitFor and await for the following to be true:
     //    - The text "Press to Get Show Data" is no longer in the DOM
@@ -27,12 +29,26 @@ describe('Stranger Things App', () => {
     //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
     //    - The text "Select A Season" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
+      await  waitFor(() => {
+        expect(screen.queryByText('Press to Get Show Data')).not.toBeInTheDocument()
+        expect(screen.getByText('Stranger Things')).toBeInTheDocument()
+        expect(screen.getByText("A love letter to the '80s classics that captivated a generation", { exact: false })).toBeInTheDocument()
+        expect(screen.getByText('Select A Season')).toBeInTheDocument()
+
+
+      })
 
     // 👉 TASK: select Season 2 from the dropdown
     // ❗ Don't forget user actions need the await keyword
     // ❗ Use the selectOptions user action
     // ❗ Grab the select element using querySelector
 
+    const selectElement = screen.getByLabelText('Select A Season')
+    await user.selectOptions(selectElement, 'Season 2')
+
+    expect(screen.getByText('Season 2, Episode 1', {exact:false})).toBeInTheDocument()
+    expect(screen.getByText('Chapter One: MADMAX', {exact:false})).toBeInTheDocument()
+    expect(screen.getByText("One year after the events with the Upside Down and the Demogorgon", {exact: false})).toBeInTheDocument()
     // 👉 TASK: create the following assertions:
     //    - The text "Season 2, Episode 1" exists in the DOM
     //    - The text "Chapter One: MADMAX" exists in the DOM
